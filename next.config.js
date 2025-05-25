5b15448caf7d78ab31c039835def272cd38d84b4
Amos Caruso <amos@1337.florist>
2025-05-25 05:30:06 -0700

    Enhance next.config.js with SWC minification, image optimization settings, and console removal in production

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   output: 'standalone',
   poweredByHeader: false,
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['carusoma.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 };
 
 module.exports = nextConfig; 
